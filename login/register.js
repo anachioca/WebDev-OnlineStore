5ba7e5d4da3e0941140d2285cba19c340c46b1a3
Paulo M Rocha <paulo.mrocha@usp.br>
2021-06-24 15:19:09 -0300

    login and register javascript

--- login/register.js
+++ login/register.js
@@ -1,13 +1,33 @@
-var email_ = document.getElementsByClassName('email_') // from register
-var password_ = document.getElementsByClassName('password_') // from register
+var email_
+var password_
 
-console.log(document.getElementsByClassName('btt-reg'))
-document.getElementsByClassName("btt-reg")[1].addEventListener("click", store)
+
+function reg_setup(){
+	email_ = document.getElementsByClassName('email_')[0] // from register
+	password_ = document.getElementsByClassName('password_') // from register
+
+	button = document.getElementsByClassName("btt-reg")[0]
+	button.addEventListener("click", store)
+}
+
+function login_setup(){
+	email_ = document.getElementsByClassName('emailL')[0] // from register
+	password_ = document.getElementsByClassName('passwordL')[0] // from register
+	console.log(email_);
+	button = document.getElementsByClassName("btt-log")[0]
+	console.log(button);
+	button.addEventListener("click", check)
+}
 
 // from register
 function store() {
-	localStorage.setItem('email_', email_.value)
-	localStorage.setItem('password_', password_.value)
+	if(password_[0].value == password_[1].value){
+		localStorage.setItem('email_', email_.value)
+		localStorage.setItem('password_', password_[0].value)
+		window.location.replace("login.html")
+	}else{
+		alert("As senhas estão diferente, por favor tente novamente")
+	}
 }
 
 // check in login
@@ -16,12 +36,14 @@ function check() {
 	var storedPw = localStorage.getItem('password_')
 
 	// entered data in login forms
-	var userEmail = document.getElementById('emailF');
-    var userPw = document.getElementById('passwordF');
+	var userEmail = document.getElementsByClassName('emailL')[0];
+    var userPw = document.getElementsByClassName('passwordL')[0];
 
-    if (userEmail.value == storedEmail && userPw == storedPw) {
+    if (userEmail.value == storedEmail && userPw.value == storedPw) {
+			localStorage.setItem("parmission", 1)
     	alert('Você está logado.')
     } else {
+			localStorage.setItem("parmission", 0)
     	alert('ERRO!')
     }
 }
