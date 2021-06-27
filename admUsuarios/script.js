39b315d25e2f6d31c4dc2f654962971768405e41
anachioca <ana.chioca@gmail.com>
2021-06-27 20:47:19 -0300

    administração de usuários pronta

--- admUsuarios/script.js
+++ admUsuarios/script.js
@@ -1,45 +1,79 @@
-const user1 = {
-    nome: "Ana Laura",
-    status: "ativo",
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ()=>{ready();})
+  } else {
+    ready()
+}
+  
+
+function ready(){
+    loadUsers();
 }
 
-const user2 = {
-    nome: "Maria Fernanda",
-    status: "ativo",
+function loadUsers(){
+    var users= localStorage.getObj("data_users");
+
+    for(let i of users){
+        console.log(i);
+        loadUser(i);
+    }
+
+    makeButtons(users);
 }
 
-const user3 = {
-    nome: "Paulo",
-    status: "banido",
+function loadUser(user){
+    var userList = document.getElementsByClassName("list-group")[0];
+    var userItem = document.createElement('div');
+
+    console.log(user.perm);
+
+    if (user.perm == 2) {
+        innerButton = `<button type="button" class="btn btn-adm btn-custom" disabled> Administrador </button>`
+    }
+    else {
+        innerButton = `<button type="button" class="btn btn-adm btn-custom"> Tornar administrador </button>`
+    }
+
+    userItem.classList.add("item")
+    var listItem = `<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                        <div class="d-flex w-100 justify-content-between"> ${user.name}
+                            <small> ${user.email} </small>
+                            ${innerButton}
+                        </div>
+                    </a>`;
+    userItem.innerHTML = listItem;
+    userList.appendChild(userItem);
 }
 
-let users = [user1, user2, user3];
+Storage.prototype.setObj = function(key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
 
-console.log(users.length)
-users.forEach(myFunction)
+Storage.prototype.getObj = function(key) {
+    return JSON.parse(this.getItem(key))
+}
 
+function makeButtons(users){
+    for(let i=0; i<users.length; i++){
+        button = document.getElementsByClassName("btn-adm")[i]
+	    button.addEventListener("click", makeAdmin)
+    }
+}
 
-function myFunction(value, index, array){
-
-    let myList = document.getElementsByClassName("list-group")[0];
-
-    let newUser = document.createElement("a");
-    newUser.className = "list-group-item list-group-item-action flex-column align-items-start"
-
-    let userName = document.createElement("div");
-    userName.className = "d-flex w-100 justify-content-between";
-    userName.innerHTML = users[index].nome;
-
-    let status = document.createElement("small");
-    status.innerHTML = users[index].status;
-
-    let myButton = document.createElement("button");
-    myButton.type = "button";
-    myButton.className = "btn btn-custom"
-    myButton.innerHTML = "Mudar Status"
-
-    userName.appendChild(status);
-    userName.appendChild(myButton);
-    newUser.appendChild(userName);
-    myList.appendChild(newUser);
+function makeAdmin(event){
+    var users = localStorage.getObj("data_users");
+    var buttonClicked = event.target
+    var emailTarget = buttonClicked.parentElement.childNodes[1].innerHTML
+    emailTarget = emailTarget.trim();
+    console.log("buttonclicked");
+    console.log(emailTarget);
+    for(let i of users){
+        console.log(i.email);
+        if(i.email === emailTarget){
+            console.log("found!")
+            i.perm = 2;
+            break;
+        }
+    }
+    localStorage.setObj("data_users", users)
+    location.reload()
 }
